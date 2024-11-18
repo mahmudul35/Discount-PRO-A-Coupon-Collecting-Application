51a308b22457e1157bc0731d9d747a945e583247
mahmudul35 <101060541+mahmudul35@users.noreply.github.com>
2024-11-18 23:20:39 +0600

    folder structure added

---
+++ src/routes/router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../layout/Root";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+export default router;
